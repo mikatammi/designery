d9afa860e9c47416fe10bbea9a5a6ee7b1863b19
Mika Tammi <mikatammi@gmail.com>
2013-04-01 13:17:38 +0300

    Disabled server listen invoke from server.js since it is not required in the development environment

--- server.js
+++ server.js
@@ -84,10 +84,10 @@ app.configure('development', function(){
 //app.get('/', express.static());
 app.get('/users', user.list);
 
-//everyauth.helpExpress(app);
+// Not required on Grunt Dev Server
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+//http.createServer(app).listen(app.get('port'), function(){
+//  console.log("Express server listening on port " + app.get('port'));
+//});
 
 module.exports = app;
